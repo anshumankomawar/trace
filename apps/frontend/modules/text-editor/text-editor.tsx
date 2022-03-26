f8ad6b363ec6f60a581bbbc16ecadb684437bac8
Anshuman Komawar <anshumankomawar@gmail.com>
2022-03-25 23:33:07 -0700

    Added home page, text editor placeholder component

---
+++ apps/frontend/modules/text-editor/text-editor.tsx
@@ -0,0 +1,3 @@
+const TextEditor = (): JSX.Element => <div className="flex min-h-screen flex-col items-center justify-center py-2">TEXT EDITOR</div>;
+
+export default TextEditor;
---
+++ apps/frontend/pages/home.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+import TextEditor from '../modules/text-editor/text-editor'
+
+const Home: NextPage = () => {
+  return (
+      <TextEditor />
+  )
+}
+
+export default Home; 
